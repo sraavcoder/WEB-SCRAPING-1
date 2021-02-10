06f5ffb158a7bed8e2ca9d1a507f92491dd7ef28
sraavcoder <61928159+sraavcoder@users.noreply.github.com>
2021-02-10 19:44:48 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -3,36 +3,34 @@ from bs4 import BeautifulSoup
 import csv
 import time
 
-START_URL = "https://exoplanets.nasa.gov/discovery/exoplanet-catalog/"
+START_URL = "https://en.wikipedia.org/wiki/List_of_brightest_stars_and_other_record_stars"
 
 browser = webdriver.Chrome("chromedriver.exe")
 browser.get(START_URL)
 time.sleep(10)
 
+
 def scrape():
-    header = ["name", "light_years_from_earth",
-              "planet_mass", "steller_magnitude", "discovery_date"]
-    planet_data = []
-    for i in range(0, 436):
-        soup = BeautifulSoup(browser.page_source, "html.parser")
-        for ul_tag in soup.find_all("ul", attrs={"class", "exoplanet"}):
-            li_tags = soup.find_all("li")
-            temp_list = []
-            for index, li_tag in enumerate(li_tags):
-                if index == 0:
-                    temp_list.append(li_tag.find_all("a")[0].contents[0])
-                else:
-                    try:
-                        temp_list.append(li_tag.contents[0])
-                    except:
-                        temp_list.append("")
-            planet_data.append(temp_list)
-        browser.find_element_by_xpath(
-            '//*[@id="primary_column"]/footer/div/div/div/nav/span[2]/a').click()
-        with open("I_Scrapped_Data_From_Web.csv", "w") as f:
-            csvwriter = csv.writer(f)
-            csvwriter.writerow(header)
-            csvwriter.writerow(planet_data)
+    header = ["v", "proper_name", "bayer_designation", "ly",
+              "spectral_class", "mass", "radius", "luminosity"]
+    stars_data = []
+    soup = BeautifulSoup(browser.page_source, "html.parser")
+    for tr_tag in soup.find_all("tr"):
+        th_tags = soup.find_all("th", attrs={"class", "headerSort"})
+        temp_list = []
+        for index, th_tag in enumerate(th_tags):
+            if index == 0:
+                temp_list.append(th_tag.find_all("a")[0].contents[0])
+            else:
+                try:
+                    temp_list.append(th_tag.contents[0])
+                except:
+                    temp_list.append("")
+        stars_data.append(temp_list)
+    with open("I_Scrapped_Data_From_Web.csv", "w") as f:
+        csvwriter = csv.writer(f)
+        csvwriter.writerow(header)
+        csvwriter.writerow(stars_data)
 
 
 scrape()
